05198b213cfd19d61d532c0da63334e40c762bda
lfm1107015 <1593598051@qq.com>
2016-09-22 11:01:32 +0800

    添加struts类库

---
+++ src/com/book/manage/action/LoginAction.java
@@ -0,0 +1,12 @@
+package com.book.manage.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport {
+
+	/**
+	 * 处理登录与注册管理员
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
