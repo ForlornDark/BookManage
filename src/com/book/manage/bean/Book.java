058e0a679a9d10e6f2e736d9499b2001359285ce
lfm1107015 <1593598051@qq.com>
2016-10-20 19:44:21 +0800

    图书bean

--- src/com/book/manage/bean/Book.java
+++ src/com/book/manage/bean/Book.java
@@ -1,8 +1,25 @@
 package com.book.manage.bean;
+
+import java.sql.Blob;
+
+/**
+ * @author lfm
+ *
+ */
 public class Book{
 	private String isbn;
 	private String bookName;
 	private String bookId;
+	private String author;
+	private String publisher;
+	private float price;
+	//复本量,复本量=库存量+已借阅
+	private int cnum;
+	//库存量
+	private int snum;
+	private String summary;
+	//图书封面图片路径，以图片格式存储在web/inf/resources下
+	private String path;
 	public String getIsbn() {
 		return isbn;
 	}
@@ -21,5 +38,47 @@ public class Book{
 	public void setBookId(String bookId) {
 		this.bookId = bookId;
 	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public int getCnum() {
+		return cnum;
+	}
+	public void setCnum(int cnum) {
+		this.cnum = cnum;
+	}
+	public int getSnum() {
+		return snum;
+	}
+	public void setSnum(int snum) {
+		this.snum = snum;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
 	
-}
+ }
