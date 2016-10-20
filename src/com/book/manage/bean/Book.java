77e4e97378e8794e945713a9c406e3acfdaeaf3d
lfm1107015 <1593598051@qq.com>
2016-10-20 19:17:45 +0800

    添加现有属性get和set

--- src/com/book/manage/bean/Book.java
+++ src/com/book/manage/bean/Book.java
@@ -3,4 +3,23 @@ public class Book{
 	private String isbn;
 	private String bookName;
 	private String bookId;
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getBookId() {
+		return bookId;
+	}
+	public void setBookId(String bookId) {
+		this.bookId = bookId;
+	}
+	
 }
